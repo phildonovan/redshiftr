a8d51919816889257d185db167050948e2885156
phildonovan <phildonovan99@protonmail.com>
2024-10-11 20:32:39 +1300

    Added in the internals scripts.

---
+++ R/redshiftr-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom paws s3
+#' @importFrom purrr walk2
+#' @importFrom readr format_csv
+## usethis namespace: end
+NULL
